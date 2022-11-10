5a5f0780fd3f91aba390f875128239b5f346a2ab
mistik9 <mistik9@icloud.com>
2022-11-10 18:45:11 +0600

    fix elements

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let popap = document.querySelector ('.popap')
+let editButton = container.querySelector('.form__submit-btn_action_edit')
+
+function editProfile() {
+ 
+  renderAdded();
+}
+
+editButton.addEventListener('click', editProfile);
+
+// let container = document.querySelector('.container');
+// let songsContainer = container.querySelector('.songs-container');
+// let addButton = container.querySelector('.form__submit-btn_action_add');
+// let resetButton = container.querySelector('.form__submit-btn_action_reset');
+
+// function renderAdded() {
+//   let songs = songsContainer.querySelectorAll('.song');
+//   let noSongsElement = container.querySelector('.no-songs');
+
+//   if (songs.length === 0) {
+//     resetButton.setAttribute('disabled', true);
+//     resetButton.classList.add('form__submit-btn_disabled');
+//     noSongsElement.classList.remove('no-songs_hidden');
+//   } else {
+//     resetButton.removeAttribute('disabled');
+//     resetButton.classList.remove('form__submit-btn_disabled');
+//     noSongsElement.classList.add('no-songs_hidden');
+//   }
+// }
+
+// function addSong() {
+//   let artist = document.querySelector('.input__text_type_artist');
+//   let song = document.querySelector('.input__text_type_song');
+
+//   songsContainer.insertAdjacentHTML('beforeend', `
+// 		<div class="song">
+//       <h4 class="song__artist">${artist.value}</h4>
+//       <p class="song__title">${song.value}</p>
+// 		  <button class="song__like"></button>
+// 		</div>
+//   `);
+//   artist.value = '';
+//     song.value = '';
+
+//   renderAdded();
+// }
+
+// addButton.addEventListener('click', addSong);
+
+// renderAdded();
