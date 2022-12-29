304cc2d96437d1aff09651a68560a0b03e2940c2
mistik9 <mistik9@icloud.com>
2022-12-29 12:46:29 +0600

    cards

---
+++ scripts/cards.js
@@ -0,0 +1,18 @@
+class Cards {
+    constructor(link, name) {
+        this._link = link;
+        this._title = title;
+        this.isliked = false;
+        this._handleCardOpen = handleCardOpen;
+    }
+_setEventListeners()
+
+    _toggleLike() {
+        this.isliked = !this.isliked
+    }
+    _deleteCard() {
+
+    }
+    _handleCardOpen() {
+
+    }
