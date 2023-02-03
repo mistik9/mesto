efb574318ac6e583848f419b00e9a276f11bc012
mistik9 <mistik9@icloud.com>
2023-02-03 12:03:46 +0600

    fix review errors

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,7 +1,7 @@
 export class Popup {
     constructor(popupSelector) {
-        this._popupSelector = document.querySelector(popupSelector);
-        this._handleEscClose = this._handleEscClose.bind(this)
+        this._popup = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this);
 
     }
 
