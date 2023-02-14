9fb82b31f276b8ef2eda645c2cb66486655e69cb
mistik9 <mistik9@icloud.com>
2023-02-14 08:53:49 +0600

    fix id avatar

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -71,7 +71,7 @@ class Api {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                avatar: data.link
+                avatar: data.avatar
             })
         })
             .then(res => {
